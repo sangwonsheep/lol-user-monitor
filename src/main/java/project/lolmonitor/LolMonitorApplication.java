712cd2a6fc08794efb95e4ca5c3634074d798d1a
sangwonsheep <sangwonsheep@gmail.com>
2025-07-06 00:44:36 +0900

    feat: 스케줄링 옵션 활성화

--- src/main/java/project/lolmonitor/LolMonitorApplication.java
+++ src/main/java/project/lolmonitor/LolMonitorApplication.java
@@ -2,8 +2,10 @@ package project.lolmonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling  // 스케줄링 활성화
 public class LolMonitorApplication {
 
 	public static void main(String[] args) {
