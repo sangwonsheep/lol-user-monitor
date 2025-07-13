74cf4e280e7b89150ae719683efca52974e60c67
sangwonsheep <sangwonsheep@gmail.com>
2025-07-14 00:22:43 +0900

    feat: 소환사 레벨 정보 확인 api 추가

--- src/main/java/project/lolmonitor/client/riot/api/RiotKoreaApi.java
+++ src/main/java/project/lolmonitor/client/riot/api/RiotKoreaApi.java
@@ -1,18 +1,21 @@
 package project.lolmonitor.client.riot.api;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
 
 import project.lolmonitor.client.riot.dto.CurrentGameInfo;
+import project.lolmonitor.client.riot.dto.Summoner;
 
 /**
- * https://developer.riotgames.com/apis#spectator-v5
- * spectator에서 사용되는 api
+ * https://kr.api.riotgames.com
  */
-public interface RiotSpectatorApi {
+public interface RiotKoreaApi {
 
 	// 게임 중 상태 확인 (실시간 게임 중인지 확인)
 	@GetExchange("/lol/spectator/v5/active-games/by-summoner/{encryptedPUUID}")
 	CurrentGameInfo getCurrentGameBySummoner(@PathVariable String encryptedPUUID);
+
+	// 소환사 레벨 정보 확인
+	@GetExchange("/lol/summoner/v4/summoners/by-puuid/{encryptedPUUID}")
+	Summoner getSummoner(@PathVariable String encryptedPUUID);
 }
